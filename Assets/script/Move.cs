23d0bdaf72950f6ec79f021d6ab3407604f2853f
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-06-12 15:29:34 +0530

    up

---
+++ Assets/script/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public Transform tank;
+	private int speed;
+	// Use this for initialization
+	void Start () {
+		speed =0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log(tank.position.x);
+
+		if(tank.position.x>8){
+			if(Input.acceleration.x<0){
+				transform.Translate(Input.acceleration.x, 0,0);
+			}
+		}else if(tank.position.x<-8){
+			if(Input.acceleration.x>0){
+				transform.Translate(Input.acceleration.x, 0,0);
+			}
+		}else{
+			transform.Translate(Input.acceleration.x, 0,0);
+		}
+	}
+}
